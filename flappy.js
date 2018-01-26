6ebcb63e24fb32e0c28021d1c1e3bc059ec4a1c3
yannis94 <33447485+yannis94@users.noreply.github.com>
2018-01-26 10:31:49 +0100

    Initial commit

---
+++ flappy.js
@@ -0,0 +1,117 @@
+// animation écran d'accueil
+function play(){
+	
+ 
+
+ 	$('.first').slideUp(400, function(){
+
+	$('section').slideDown();
+ 	$('section').css({
+ 		display: 'flex',
+ 		justifyContent: 'center',
+ 		alignItems: 'center'
+ 	})
+
+ 	});
+
+ 	$('#btn').hide();
+
+
+ 	//mouvement tuyeau
+function animpipe(){
+
+ 	$('#pipe').animate({right: '+=950px'}, 7000, 'linear', function hauteur(){
+
+ 		var htr = Math.floor(Math.random()*500);
+
+ 		$('#pipe').css('top', htr)
+ 	}  )
+
+ 	}
+ };
+
+
+	
+
+
+
+
+	
+
+	
+
+ 	
+// function a(){
+//  		alert('aïïïe')
+//  	};
+
+
+
+
+
+document.addEventListener("keydown",saut, false); 
+function saut(){
+// alert (parseInt(event.keycode));
+
+
+
+
+//flappy projet
+
+	var flappy = $('#bird') ;
+
+	var fleche = event.which ;
+
+	if (fleche ==38) { // fleche haut
+		flappy.animate({marginBottom: '+=100px'}, 50); }
+
+	if (fleche ==40) { // fleche bas
+		flappy.animate({marginTop: '+=100px'}, 50); }
+
+	if (fleche ==39) { //fleche gauche
+		flappy.animate({marginLeft: '+=100px'}, 50); }
+
+	if (fleche ==37) { //fleche droite
+		flappy.animate({marginRight: '+=100px'}, 50); }
+
+
+	}
+
+
+
+
+// flappy classic
+	
+
+
+	// var flappy = $('#bird');
+
+	// var espace = event.which ;
+
+
+
+	
+
+	// if (espace ==32) {
+	// 	$(flappy).animate({marginBottom: '+=100px'}, 200);}
+		
+ 
+//  	else {
+
+// // // function gravité(){
+
+
+// 	// $('#bird').animate({top : '+=1500px'}, 7000, 'linear');} 
+// 	$(flappy).animate({right: '+=1000px'}, 7000, 'linear');}
+
+// 	setTimeout('gravité', 800);
+
+// }
+
+//}
+
+
+
+
+
+
